eb9a913fc9e31596db498d6f8c612555bfed70f4
hexclean <hexclean@hexcleans-MacBook-Pro.local>
2020-07-01 22:54:46 +0300

    localstorage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,8 +4,8 @@ import "../css/Home.css";
 import axios from "axios";
 const Login = () => {
   const [email, setEmail] = useState("");
-  const [login] = useState(false);
   const [password, setPassword] = useState("");
+  const tokenLocal = localStorage.getItem("x-auth");
 
   const loginpress = () => {
     axios
@@ -14,31 +14,49 @@ const Login = () => {
         password: password,
       })
       .then((response) => {
-        const token = response.data.token;
-
-        localStorage.setItem("x-auth", token);
-        console.log(token);
+        try {
+          const token = response.data.token;
+          localStorage.setItem(
+            "x-auth",
+            JSON.stringify({
+              login: true,
+              token: token,
+            })
+          );
+          console.log(token);
+          console.log("react localstorage: ", localStorage);
+        } catch (error) {
+          console.log(error);
+        }
       });
   };
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <div>adsd</div>
-      <input
-        type="text"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit" onClick={loginpress}>
-        Log In
-      </button>
+      {!tokenLocal ? (
+        <div>
+          <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div>adsd</div>
+          <input
+            type="text"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" onClick={loginpress}>
+            Log In
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h1>dsadadasd</h1>
+        </div>
+      )}
     </div>
   );
 };
