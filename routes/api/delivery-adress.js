f312f4217bea0c55baeb8f26e1f137f54bd23aea
hexclean <hexclean@hexcleans-MacBook-Pro.local>
2020-07-12 23:03:59 +0300

    API

--- routes/api/delivery-adress.js
+++ routes/api/delivery-adress.js
@@ -5,7 +5,7 @@ const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 
-// @route    GET api/delivery adress
+// @route    POST api/delivery adress
 // @desc     Create delivery adress
 // @access   Private
 router.post("/", auth, async (req, res) => {
@@ -28,16 +28,40 @@ router.post("/", auth, async (req, res) => {
   }
 });
 
-exports.apiCreate = function (req, res) {
-  let post = new Post(req.body, req.apiUser._id);
-  post
-    .create()
-    .then(function (newId) {
-      res.json(newId);
-    })
-    .catch(function (errors) {
-      res.json(errors);
+// @route    GET api/delivery adress
+// @desc     Create delivery adress
+// @access   Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const deliveryAdress = await UserDeliveryAdress.findAll({
+      where: { userId: req.user.id },
     });
-};
+
+    res.json(deliveryAdress);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("server error");
+  }
+});
+
+// @route    GET api/delivery adress
+// @desc     Delete delivery adress
+// @access   Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const deliveryAdress = await UserDeliveryAdress.findByPk(req.param.id);
+
+    if (!deliveryAdress) {
+      return res.status(404).json({ msg: "Delivery Adress not found" });
+    }
+    res.json(deliveryAdress);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === "ObjecId") {
+      return res.status(404).json({ msg: "Delivery Adress not found" });
+    }
+    res.status(500).send("server error");
+  }
+});
 
 module.exports = router;
