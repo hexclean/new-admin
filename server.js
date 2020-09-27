668947f9ab7e93b4d683ecacb2948d25338b5f2c
Nagy Ervin <nagyervin@Nagys-MacBook-Pro.local>
2020-09-27 17:40:01 +0300

    db teszt

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use((req, res, next) => {
   next();
 });
 
-const db = new Sequelize("foodnet", "root", "y7b5uwFOODNET", {
-  host: "localhost",
+const db = new Sequelize("x0nfgxnh2o2y198n", "lphjb6a4r5qdgfgz", "qche3i7iay5xtmpo", {
+  host: "durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   dialect: "mysql",
 });
 
