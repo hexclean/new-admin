4e2d740c5bafd8f798c9ddc9eca7c2b1a0c769f6
Nagy Ervin <nagyervin@Nagys-MacBook-Pro.local>
2020-09-27 17:49:41 +0300

    db teszt

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 const db = new Sequelize("x0nfgxnh2o2y198n", "lphjb6a4r5qdgfgz", "qche3i7iay5xtmpo", {
   host: "durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  dialect: "mysql",
+  port: 3306
 });
 
 const sessionStore = new SequelizeStore({
