d709e41e22f3acc45b35ff3d4bfebd84a5f013e8
Nagy Ervin <nagyervin@Nagys-MacBook-Pro.local>
2020-09-28 14:02:06 +0300

    set heroku db

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 const db = {};
-const sequelize = new Sequelize("foodnet", "root", "y7b5uwFOODNET", {
+const sequelize = new Sequelize("x0nfgxnh2o2y198n", "lphjb6a4r5qdgfgz", "qche3i7iay5xtmpo", {
   dialect: "mysql",
-  host: "localhost",
+  host: "durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 });
 
 db.sequelize = sequelize;
