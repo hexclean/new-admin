7db750af557278a35614f72022fc1b2b1d2f3e85
hexclean <hexclean@192-168-0-105.rdsnet.ro>
2020-10-05 13:08:53 +0300

    new db password

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const db = {};
 const sequelize = new Sequelize(
   "x0nfgxnh2o2y198n",
   "lphjb6a4r5qdgfgz",
-  "qche3i7iay5xtmpo",
+  "kekdxp11rr1e5705",
   {
     dialect: "mysql",
     host: "durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
