6e2c26acd82f9f84d5bef8542eda349e1b1f5802
hexclean <erdosjozsef20@gmail.com>
2020-11-16 12:34:49 +0200

    filters

--- util/database.js
+++ util/database.js
@@ -1,19 +1,19 @@
 const Sequelize = require("sequelize");
 const db = {};
-// const sequelize = new Sequelize(
-//   "x0nfgxnh2o2y198n",
-//   "lphjb6a4r5qdgfgz",
-//   "li4e0dhgofk9vhtt",
-//   {
-//     dialect: "mysql",
-//     host: "durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//   }
-// );
+const sequelize = new Sequelize(
+  "x0nfgxnh2o2y198n",
+  "lphjb6a4r5qdgfgz",
+  "li4e0dhgofk9vhtt",
+  {
+    dialect: "mysql",
+    host: "durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  }
+);
 
-const sequelize = new Sequelize("foodnet3", "root", "", {
-  dialect: "mysql",
-  host: "localhost",
-});
+// const sequelize = new Sequelize("foodnet3", "root", "", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
