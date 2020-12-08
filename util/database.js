a547bd2e1e921fe9642c8e88909629f2e056a6fd
hexclean <erdosjozsef20@gmail.com>
2020-12-08 16:57:38 +0200

    delivery address id

--- util/database.js
+++ util/database.js
@@ -1,16 +1,16 @@
 const Sequelize = require("sequelize");
 const db = {};
 
-// sequelize = new Sequelize("defaultdb", "doadmin", "s27c8agot7l7a13z", {
-//   dialect: "mysql",
-//   host: "foodnet-database-do-user-8133521-0.b.db.ondigitalocean.com",
-//   port: 25060,
-// });
-
-const sequelize = new Sequelize("foodnet45", "root", "", {
+sequelize = new Sequelize("defaultdb", "doadmin", "s27c8agot7l7a13z", {
   dialect: "mysql",
-  host: "localhost",
+  host: "foodnet-database-do-user-8133521-0.b.db.ondigitalocean.com",
+  port: 25060,
 });
+
+// const sequelize = new Sequelize("foodnet45", "root", "", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
