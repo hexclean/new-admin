e8f4c074b23b9ffea3dc5c22a900bac28fcf9357
hexclean <erdosjozsef20@gmail.com>
2020-12-31 15:41:14 +0200

    fix order detail

--- admin/controllers/order.js
+++ admin/controllers/order.js
@@ -395,188 +395,382 @@ exports.getAcceptedOrders = async (req, res, next) => {
   });
 };
 exports.getEditOrder = async (req, res, next) => {
-  const editMode = req.query.edit;
   const orderId = req.params.orderId;
-  try {
-    if (!editMode) {
-      return res.redirect("/");
-    }
+  let result2 = [];
+  const orders = await Order.findAll({
+    // order: [["createdAt", "DESC"]],
+    where: {
+      id: orderId,
+      restaurantId: req.admin.id,
+    },
 
-    await Order.findByPk(orderId).then((order) => {
-      if (!order) {
-        return res.redirect("/");
-      }
-    });
+    include: [
+      {
+        model: OrderItem,
 
-    const order = await Order.findAll({
-      // order: [["createdAt", "ASC"]],
-
-      where: {
-        id: orderId,
-        restaurantId: req.admin.id,
+        include: [
+          {
+            model: OrderItemExtra,
+            include: [
+              {
+                model: Extra,
+                include: [{ model: ExtraTranslation }],
+              },
+            ],
+          },
+          {
+            model: Variant,
+            include: [
+              {
+                model: ProductFinal,
+                where: { active: 1 },
+                include: [
+                  {
+                    model: Product,
+                    include: [{ model: ProductTranslation }],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
       },
+      { model: OrderDeliveryAddress },
+      { model: User },
 
-      include: [
-        {
-          model: User,
-          model: OrderItem,
+      {
+        model: LocationName,
+        include: [
+          {
+            model: LocationNameTranslation,
+            where: { languageId: 2 },
+          },
+        ],
+      },
+    ],
+  });
 
-          include: [
-            {
-              model: OrderItemExtra,
-            },
-            {
-              model: Variant,
-              include: [
-                {
-                  model: ProductFinal,
-                  include: [
-                    {
-                      model: Product,
-                      include: [{ model: ProductTranslation }],
-                    },
-                  ],
-                },
-                {
-                  model: ProductVariantsExtras,
-                  include: [
-                    { model: Extra, include: [{ model: ExtraTranslation }] },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-        { model: OrderDeliveryAddress },
-        { model: User },
-        {
-          model: LocationName,
-          include: [
-            {
-              model: LocationNameTranslation,
-            },
-          ],
-        },
-      ],
-    });
-    let extras = [];
-    const resultWithAll = [];
-    let variants = order[0].OrderItems;
-    for (let j = 0; j < variants.length; j++) {
-      extras = variants[j].OrderItemExtras;
+  let extras = [];
+  let totalPriceFinal;
+  let cutlery;
+  let take;
+  let userName;
+  let orderCity;
+  let orderStreet;
+  let orderHouseNumber;
+  let orderFloor;
+  let orderDoorNumber;
+  let orderPhoneNumber;
+  let orderCreated;
+  let orderIds;
+  let extrasArray = [];
+  if (orders.length != 0) {
+    for (let i = 0; i < orders.length; i++) {
+      const resultWithAll = [];
+      let orderItems = orders[i].OrderItems;
 
-      for (let k = 0; k < extras.length; k++) {
-        let totalProductPrice = 0;
-        let totalExtraPrice = 0;
+      for (let j = 0; j < orderItems.length; j++) {
+        extras = orderItems[j].OrderItemExtras;
 
-        totalProductPrice +=
-          parseFloat(variants[j].variantPrice) * parseInt(variants[j].quantity);
-        totalExtraPrice +=
-          parseFloat(extras[k].extraPrice) * parseInt(extras[k].quantity);
+        let prodFin = orderItems[j].Variant.ProductFinals;
+        for (let h = 0; h < prodFin.length; h++) {
+          console.log("extras[j]", extras);
+          if (extras.length == 0) {
+            let totalProductPrice = 0;
 
-        const items = {
-          product_id: variants[j].Variant.ProductFinals[j].productId,
-          product_quantity: variants[j].quantity,
-          product_price: variants[j].variantPrice,
-          product_name:
-            variants[j].Variant.ProductFinals[j].Product.ProductTranslations[0]
-              .title,
-          extra_id: extras[k].extraId,
-          extra_quantity: extras[k].quantity,
-          extra_price: extras[k].extraPrice,
-          extra_name:
-            variants[j].Variant.ProductVariantsExtras[k].Extra
-              .ExtraTranslations[0].name,
-          total_product_price: totalProductPrice,
-          total_extra_price: totalExtraPrice,
-        };
+            totalProductPrice +=
+              parseFloat(orderItems[j].variantPrice) *
+              parseInt(orderItems[j].quantity);
+            const items = {
+              product_id: prodFin[h].productId,
+              product_quantity: orderItems[j].quantity,
+              message: orderItems[j].message,
+              product_price: orderItems[j].variantPrice,
+              product_name: prodFin[h].Product.ProductTranslations[0].title,
+              total_product_price: totalProductPrice,
+            };
 
-        resultWithAll.push(items);
-      }
-    }
-    const merged = resultWithAll.reduce(
-      (
-        r,
-        {
-          product_id,
-          product_quantity,
-          product_price,
-          product_name,
-          total_product_price,
+            resultWithAll.push(items);
+          } else {
+            for (let k = 0; k < extras.length; k++) {
+              extrasArray.push(extras[k]);
+              let totalProductPrice = 0;
+              let totalExtraPrice = 0;
 
-          ...rest
+              totalProductPrice +=
+                parseFloat(orderItems[j].variantPrice) *
+                parseInt(orderItems[j].quantity);
+              totalExtraPrice +=
+                parseFloat(extras[k].extraPrice) * parseInt(extras[k].quantity);
+              const items = {
+                product_id: prodFin[h].productId,
+                product_quantity: orderItems[j].quantity,
+                product_price: orderItems[j].variantPrice,
+                product_name: prodFin[h].Product.ProductTranslations[0].title,
+                extra_id: extras[k].extraId,
+                extra_quantity: extras[k].quantity,
+                extra_price: extras[k].extraPrice,
+                extra_name: extras[k].Extra.ExtraTranslations[0].name,
+                total_product_price: totalProductPrice,
+                total_extra_price: totalExtraPrice,
+                message: orderItems[j].message,
+              };
+
+              resultWithAll.push(items);
+            }
+          }
         }
-      ) => {
-        const key = `${product_id}-${product_quantity}-${product_price}-${product_name}-${total_product_price}`;
-        r[key] = r[key] || {
-          product_id,
-          product_quantity,
-          product_price,
-          product_name,
-          total_product_price,
+      }
 
-          extras: [],
-        };
-        r[key]["extras"].push(rest);
-        return r;
-      },
-      {}
-    );
+      const merged = resultWithAll.reduce(
+        (
+          r,
+          {
+            product_id,
+            product_quantity,
+            product_price,
+            product_name,
+            total_product_price,
+            message,
+            ...rest
+          }
+        ) => {
+          const key = `${product_id}-${product_quantity}-${product_price}-${product_name}-${total_product_price}-${message}`;
+          r[key] = r[key] || {
+            product_id,
+            product_quantity,
+            product_price,
+            product_name,
+            total_product_price,
+            message,
+            extras: [],
+          };
+          r[key]["extras"].push(rest);
+          return r;
+        },
+        {}
+      );
 
-    const result = Object.values(merged);
-
-    let totalPriceFinal;
-    let cutlery;
-    let take;
-    let userName;
-    let orderCity;
-    let orderStreet;
-    let orderHouseNumber;
-    let orderFloor;
-    let orderDoorNumber;
-    let orderPhoneNumber;
-    let orderCreated;
-    let orderIds;
-    totalPriceFinal = order[0].totalPrice;
-    cutlery = order[0].cutlery;
-    take = order[0].take;
-    //   orderCity = order[0].OrderDeliveryAddress;
-    orderStreet = order[0].OrderDeliveryAddress.street;
-    orderHouseNumber = order[0].OrderDeliveryAddress.houseNumber;
-    orderFloor = order[0].OrderDeliveryAddress.floor;
-    orderDoorNumber = order[0].OrderDeliveryAddress.doorNumber;
-    orderPhoneNumber = order[0].OrderDeliveryAddress.phoneNumber;
-    orderCreated = order[0].createdAt;
-    userName = order[0].User.fullName;
-    orderIds = order[0].id;
-
-    res.render("order/edit-order", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      editing: editMode,
-      order: order,
-      orderStreet: orderStreet,
-      orderHouseNumber: orderHouseNumber,
-      orderFloor: orderFloor,
-      orderDoorNumber: orderDoorNumber,
-      orderPhoneNumber: orderPhoneNumber,
-      orderCreated: orderCreated,
-      userName: userName,
-      cutlery: cutlery,
-      take: take,
-      orderIds: orderIds,
-      variants: order[0].OrderItems,
-      extras: extras,
-      result: result,
-      userEmail: order[0].User.email,
-    });
-  } catch (err) {
-    console.log(err);
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
+      const result = Object.values(merged);
+      result2 = result;
+      orders[i].products = result;
+    }
+    totalPriceFinal = orders[0].totalPrice;
+    cutlery = orders[0].cutlery;
+    take = orders[0].take;
+    //   orderCity = orders[0].OrderDeliveryAddress;
+    orderStreet = orders[0].OrderDeliveryAddress.street;
+    orderHouseNumber = orders[0].OrderDeliveryAddress.houseNumber;
+    orderFloor = orders[0].OrderDeliveryAddress.floor;
+    orderDoorNumber = orders[0].OrderDeliveryAddress.doorNumber;
+    orderPhoneNumber = orders[0].OrderDeliveryAddress.phoneNumber;
+    orderCreated = orders[0].createdAt;
+    userName = orders[0].User.fullName;
+    orderIds = orders[0].id;
   }
+  res.render("order/edit-order", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
+    orders: orders,
+    orderStreet: orderStreet,
+    orderHouseNumber: orderHouseNumber,
+    orderFloor: orderFloor,
+    orderDoorNumber: orderDoorNumber,
+    orderPhoneNumber: orderPhoneNumber,
+    orderCreated: orderCreated,
+    userName: userName,
+    cutlery: cutlery,
+    take: take,
+    orderIds: orderIds,
+    extras: extras,
+    result: result2,
+    userEmail: orders[0].User.email,
+  });
 };
+// exports.getEditOrder = async (req, res, next) => {
+//   const editMode = req.query.edit;
+//   const orderId = req.params.orderId;
+//   try {
+//     if (!editMode) {
+//       return res.redirect("/");
+//     }
+
+//     await Order.findByPk(orderId).then((order) => {
+//       if (!order) {
+//         return res.redirect("/");
+//       }
+//     });
+
+//     const order = await Order.findAll({
+//       // order: [["createdAt", "ASC"]],
+
+//       where: {
+//         id: orderId,
+//         restaurantId: req.admin.id,
+//       },
+
+//       include: [
+//         {
+//           model: User,
+//           model: OrderItem,
+
+//           include: [
+//             {
+//               model: OrderItemExtra,
+//             },
+//             {
+//               model: Variant,
+//               include: [
+//                 {
+//                   model: ProductFinal,
+//                   include: [
+//                     {
+//                       model: Product,
+//                       include: [{ model: ProductTranslation }],
+//                     },
+//                   ],
+//                 },
+//                 {
+//                   model: ProductVariantsExtras,
+//                   include: [
+//                     { model: Extra, include: [{ model: ExtraTranslation }] },
+//                   ],
+//                 },
+//               ],
+//             },
+//           ],
+//         },
+//         { model: OrderDeliveryAddress },
+//         { model: User },
+//         {
+//           model: LocationName,
+//           include: [
+//             {
+//               model: LocationNameTranslation,
+//             },
+//           ],
+//         },
+//       ],
+//     });
+//     let extras = [];
+//     const resultWithAll = [];
+//     let variants = order[0].OrderItems;
+//     for (let j = 0; j < variants.length; j++) {
+//       extras = variants[j].OrderItemExtras;
+
+//       for (let k = 0; k < extras.length; k++) {
+//         let totalProductPrice = 0;
+//         let totalExtraPrice = 0;
+
+//         totalProductPrice +=
+//           parseFloat(variants[j].variantPrice) * parseInt(variants[j].quantity);
+//         totalExtraPrice +=
+//           parseFloat(extras[k].extraPrice) * parseInt(extras[k].quantity);
+
+//         const items = {
+//           product_id: variants[j].Variant.ProductFinals[j].productId,
+//           product_quantity: variants[j].quantity,
+//           product_price: variants[j].variantPrice,
+//           product_name:
+//             variants[j].Variant.ProductFinals[j].Product.ProductTranslations[0]
+//               .title,
+//           extra_id: extras[k].extraId,
+//           extra_quantity: extras[k].quantity,
+//           extra_price: extras[k].extraPrice,
+//           extra_name:
+//             variants[j].Variant.ProductVariantsExtras[k].Extra
+//               .ExtraTranslations[0].name,
+//           total_product_price: totalProductPrice,
+//           total_extra_price: totalExtraPrice,
+//         };
+
+//         resultWithAll.push(items);
+//       }
+//     }
+//     const merged = resultWithAll.reduce(
+//       (
+//         r,
+//         {
+//           product_id,
+//           product_quantity,
+//           product_price,
+//           product_name,
+//           total_product_price,
+
+//           ...rest
+//         }
+//       ) => {
+//         const key = `${product_id}-${product_quantity}-${product_price}-${product_name}-${total_product_price}`;
+//         r[key] = r[key] || {
+//           product_id,
+//           product_quantity,
+//           product_price,
+//           product_name,
+//           total_product_price,
+
+//           extras: [],
+//         };
+//         r[key]["extras"].push(rest);
+//         return r;
+//       },
+//       {}
+//     );
+
+//     const result = Object.values(merged);
+
+//     let totalPriceFinal;
+//     let cutlery;
+//     let take;
+//     let userName;
+//     let orderCity;
+//     let orderStreet;
+//     let orderHouseNumber;
+//     let orderFloor;
+//     let orderDoorNumber;
+//     let orderPhoneNumber;
+//     let orderCreated;
+//     let orderIds;
+//     totalPriceFinal = order[0].totalPrice;
+//     cutlery = order[0].cutlery;
+//     take = order[0].take;
+//     //   orderCity = order[0].OrderDeliveryAddress;
+//     orderStreet = order[0].OrderDeliveryAddress.street;
+//     orderHouseNumber = order[0].OrderDeliveryAddress.houseNumber;
+//     orderFloor = order[0].OrderDeliveryAddress.floor;
+//     orderDoorNumber = order[0].OrderDeliveryAddress.doorNumber;
+//     orderPhoneNumber = order[0].OrderDeliveryAddress.phoneNumber;
+//     orderCreated = order[0].createdAt;
+//     userName = order[0].User.fullName;
+//     orderIds = order[0].id;
+
+//     res.render("order/edit-order", {
+//       pageTitle: "Edit Product",
+//       path: "/admin/edit-product",
+//       editing: editMode,
+//       order: order,
+//       orderStreet: orderStreet,
+//       orderHouseNumber: orderHouseNumber,
+//       orderFloor: orderFloor,
+//       orderDoorNumber: orderDoorNumber,
+//       orderPhoneNumber: orderPhoneNumber,
+//       orderCreated: orderCreated,
+//       userName: userName,
+//       cutlery: cutlery,
+//       take: take,
+//       orderIds: orderIds,
+//       variants: order[0].OrderItems,
+//       extras: extras,
+//       result: result,
+//       userEmail: order[0].User.email,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     const error = new Error(err);
+//     error.httpStatusCode = 500;
+//     return next(error);
+//   }
+// };
 exports.getDeletedOrders = async (req, res, next) => {
   const orders = await Order.findAll({
     where: { orderStatusId: 3, restaurantId: req.admin.id },
