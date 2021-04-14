5c8859ca9cbcdb4b6dc61b5ed020d1b503309d9b
Erdős József <erdosjozsef@Erdoss-MacBook-Pro.local>
2021-04-14 20:36:57 +0300

    jquery fixed

--- util/database.js
+++ util/database.js
@@ -1,17 +1,17 @@
 const Sequelize = require("sequelize");
 const db = {};
 
-sequelize = new Sequelize("defaultdb", "doadmin", "s27c8agot7l7a13z", {
-  dialect: "mysql",
-  host: "foodnet-database-do-user-8133521-0.b.db.ondigitalocean.com",
-  port: 25060,
-});
-
-// sequelize = new Sequelize("defaultdb", "doadmin", "uzg7vmow9hgs0dlw", {
+// sequelize = new Sequelize("defaultdb", "doadmin", "s27c8agot7l7a13z", {
 //   dialect: "mysql",
-//   host: "staging-db-do-user-8133521-0.b.db.ondigitalocean.com",
+//   host: "foodnet-database-do-user-8133521-0.b.db.ondigitalocean.com",
 //   port: 25060,
 // });
+
+sequelize = new Sequelize("defaultdb", "doadmin", "uzg7vmow9hgs0dlw", {
+  dialect: "mysql",
+  host: "staging-db-do-user-8133521-0.b.db.ondigitalocean.com",
+  port: 25060,
+});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
